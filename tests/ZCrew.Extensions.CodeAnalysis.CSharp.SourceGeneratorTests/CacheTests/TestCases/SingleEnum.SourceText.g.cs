54db37aab5b485dcb17224003ad1391c488cad8e
Matt Zawislak <mattwzawislak@gmail.com>
2026-02-13 17:34:08 -0700

    #2 - Add in SourceText extension to add definition for abstractions

--- tests/ZCrew.Extensions.CodeAnalysis.CSharp.SourceGeneratorTests/CacheTests/TestCases/SingleEnum.SourceText.g.cs
+++ tests/ZCrew.Extensions.CodeAnalysis.CSharp.SourceGeneratorTests/CacheTests/TestCases/SingleEnum.SourceText.g.cs
@@ -3,6 +3,7 @@
 #pragma warning disable 0618, 1591, 8019
 
 using System.Text;
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Text;
 
 namespace CacheTests;
@@ -25,4 +26,7 @@ internal static class TestEnumSourceText
         """,
         Encoding.UTF8
     );
+    
+    public static void AddTestEnumDefinition(this IncrementalGeneratorPostInitializationContext context)
+        => context.AddSource("CacheTests.TestEnum.g.cs", SourceText);
 }
